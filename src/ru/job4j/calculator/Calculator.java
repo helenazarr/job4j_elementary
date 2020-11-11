6d908088e63904d5c5488355739e8938cf4298c9
Elena Zarubina <ezarubina@croc.ru>
2020-11-11 19:09:28 +0300

    9.2. Checkstyle [#404631]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -5,10 +5,10 @@ public class Calculator {
         int one = 1;
         int two = 2;
         int six = 6;
-        int four =4;
+        int four = 4;
         int five = 5;
         int onePlusTwo = one + two;
-        int sixDivTwo = six/two;
+        int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
         System.out.println(onePlusTwo);
