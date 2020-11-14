0a42f8258545d7a5509a22aebac1c5b19fbeecca
Elena Zarubina <ezarubina@croc.ru>
2020-11-14 22:50:21 +0300

    6.0.2. Заполнение массива. [#404596]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Elena";
+        names[1] = "Alex";
+        names[2] = "Ellie";
+        names[3] = "Michael";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
