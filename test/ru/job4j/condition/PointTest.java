8d8928a52dcd01d68fde3d995b69bcca1cbae88b
Elena Zarubina <ezarubina@croc.ru>
2020-11-20 22:23:41 +0300

    4. Расстояние между точками в трехмерном пространстве. [#404773]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -3,48 +3,41 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
 public class PointTest {
     @Test
     public void when00to20then2() {
-        int expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
+        assertThat(out, closeTo(2, 0.001));
     }
 
     @Test
     public void when35to93then6() {
-        double expected = 6.32;
-        int x1 = 3;
-        int y1 = 5;
-        int x2 = 9;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(3, 5);
+        Point b = new Point(9, 3);
+        double out = a.distance(b);
+        assertThat(out, closeTo(6.3246, 0.001));
     }
 
     @Test
-    public void when71to28then8() {
-        double expected = 8.6;
-        int x1 = 7;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 8;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void when803to937then10() {
+        Point a = new Point(8, 0, 3);
+        Point b = new Point(9, 3, 7);
+        double out = a.distance(b);
+        assertThat(out, closeTo(3.1622, 0.001));
     }
 
     @Test
-    public void when59to60then9() {
-        double expected = 9.06;
-        int x1 = 5;
-        int y1 = 9;
-        int x2 = 6;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void when803to069then3() {
+        Point a = new Point(8, 0, 3);
+        Point b = new Point(0, 6, 9);
+        double out = a.distance(b);
+        assertThat(out, closeTo(10, 0.001));
     }
+
+
 }
